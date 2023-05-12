9492e5407d6046acc8095f5fa3f33f516c0d9224
Denis Fesenko <fesenkoden@gmail.com>
2023-05-13 00:37:47 +0200

    LeetCode Valid Parentheses

---
+++ com/denisfesenko/ValidParentheses.java
@@ -0,0 +1,46 @@
+package com.denisfesenko;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * 20. Valid Parentheses
+ */
+public class ValidParentheses {
+    public static void main(String[] args) {
+        System.out.println(isValid("()[]{}")); //true
+        System.out.println(isValid("()")); //true
+        System.out.println(isValid("(]")); //false
+        System.out.println(isValid("([)]")); //false
+        System.out.println(isValid("{[]}")); //true
+        System.out.println(isValid("[")); //false
+        System.out.println(isValid("((")); //false
+        System.out.println(isValid("(){}}{")); //false
+        System.out.println(isValid("))")); //false
+    }
+
+    public static boolean isValid(String s) {
+        if (s.length() < 2) {
+            return false;
+        }
+
+        Map<Character, Character> characterMap = new HashMap<>();
+        characterMap.put('(', ')');
+        characterMap.put('{', '}');
+        characterMap.put('[', ']');
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (Character c : s.toCharArray()) {
+            if (characterMap.containsKey(c)) {
+                deque.add(c);
+            } else if (deque.isEmpty() || !Objects.equals(c, characterMap.get(deque.pollLast()))) {
+                return false;
+            }
+        }
+        return deque.isEmpty();
+    }
+}
