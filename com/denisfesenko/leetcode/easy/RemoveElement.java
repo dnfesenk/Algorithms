04ff41027f5e72061565c765cf559684565dec32
Denis Fesenko <fesenkoden@gmail.com>
2023-05-15 02:59:29 +0200

    LeetCode Remove Element

---
+++ com/denisfesenko/leetcode/easy/RemoveElement.java
@@ -0,0 +1,28 @@
+package com.denisfesenko.leetcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * 27. Remove Element
+ */
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] array = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println(removeElement(array, 2));
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
+}
