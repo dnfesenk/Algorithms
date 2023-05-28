7a8dfe19d7f8e78630e3c07236783a47c2e0b012
Denis Fesenko <fesenkoden@gmail.com>
2023-05-29 00:38:01 +0200

    LeetCode Merge Sorted Array

---
+++ com/denisfesenko/leetcode/easy/MergeSortedArray.java
@@ -0,0 +1,29 @@
+package com.denisfesenko.leetcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * 88. Merge Sorted Array
+ */
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int[] nums2 = new int[]{2, 5, 6};
+        merge(nums1, 3, nums2, 3);
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+        }
+    }
+}
